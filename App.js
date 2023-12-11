bd2961f67c960b103c967ddd0efc450fecc76f3e
Caleb John <johncaleb022@gmail.com>
2023-12-11 13:27:30 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-
 import AppNavigation from './navigation/appNavigation';
 
 // import { NavigationContainer } from '@react-navigation/native';
