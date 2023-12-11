8dd5cee29fe1eee24e135c55286f21cc3ed5bd4a
Caleb John <johncaleb022@gmail.com>
2023-12-11 13:28:36 -0800

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import AppNavigation from './navigation/appNavigation';
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // import HomeScreen from './screens/HomeScreen';
 // import GameStore from './screens/gameStore';
-
 // const Tab = createBottomTabNavigator();
 
 export default function App() {
