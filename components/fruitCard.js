072bea3785f95a1e96166dfe8108dc0179cdb0ac
Caleb John <johncaleb022@gmail.com>
2026-03-15 20:00:58 +0100

    Update fruitCard.js

--- components/fruitCard.js
+++ components/fruitCard.js
@@ -1,3 +1,13 @@
+
+
+
+
+
+
+
+
+
+
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import { HeartIcon } from 'react-native-heroicons/solid'
