3b4a86d4b0e96c9ef94005b1a64232f8dc47ed23
Caleb John <johncaleb022@gmail.com>
2026-04-11 12:22:29 +0100

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,5 @@
 
 
-
 import { View, Text, TouchableOpacity, Image, ScrollView, Touchable } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
